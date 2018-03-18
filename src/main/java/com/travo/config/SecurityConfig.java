33ebd8651c0c7a25fb71a6fd18fcd049d707c0d5
licod1997 <licod1997@gmail.com>
2018-03-18 13:54:35 +0900

    Adding templates to test security

--- src/main/java/com/travo/config/SecurityConfig.java
+++ src/main/java/com/travo/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
+                .logoutSuccessUrl("/login")
                 .and()
                 .exceptionHandling()
                 .accessDeniedPage("/access-denied")
--- src/main/java/com/travo/controller/LoginController.java
+++ src/main/java/com/travo/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class LoginController {
@@ -14,4 +15,17 @@ public class LoginController {
     public ResponseEntity authCheck() {
         return ResponseEntity.status(HttpStatus.OK).body("Logged in!");
     }
+
+    @GetMapping("/login")
+    public ModelAndView loginPage(ModelAndView model) {
+        model.setViewName("login");
+        return model;
+    }
+
+    @GetMapping("/home")
+    public ModelAndView homePage(ModelAndView model) {
+        model.setViewName("home");
+        return model;
+    }
+
 }
